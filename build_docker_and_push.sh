07f9fc9b8249f20b9f7b2fc10053107b733469ea
fabiocmazzo <fabiomazzo@gmail.com>
2022-06-01 13:20:36 -0300

    Increase timeout

--- build_docker_and_push.sh
+++ build_docker_and_push.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-sudo docker build -t scylladbdump:latest .
-sudo docker tag scylladbdump:latest klinkbr/wisecapture:latest
-sudo docker push klinkbr/wisecapture:latest
+sudo docker build -t scylladbdump:v2 .
+sudo docker tag scylladbdump:v2 klinkbr/wisecapture:dump_v2
+sudo docker push klinkbr/wisecapture:dump_v2
